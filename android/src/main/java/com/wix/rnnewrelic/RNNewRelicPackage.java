85feca86dfd81e69f6ac03f4bad75ff9025851c3
Aleksey <pohodnyaa@gmail.com>
2017-08-08 11:49:58 +0700

    Fix Compilation Error in React Native 0.47.0

--- android/src/main/java/com/wix/rnnewrelic/RNNewRelicPackage.java
+++ android/src/main/java/com/wix/rnnewrelic/RNNewRelicPackage.java
@@ -20,7 +20,6 @@ public class RNNewRelicPackage implements ReactPackage {
         return Arrays.<NativeModule>asList(new RNNewRelic(reactContext));
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
