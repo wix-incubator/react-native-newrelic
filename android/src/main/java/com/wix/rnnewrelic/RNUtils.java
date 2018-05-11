a1b64afe379b8da6d8a23948288a13c061d5d404
william <william.s@kimoby.com>
2018-05-11 13:11:57 -0400

    Fixed build for RN

--- android/src/main/java/com/wix/rnnewrelic/RNUtils.java
+++ android/src/main/java/com/wix/rnnewrelic/RNUtils.java
@@ -40,7 +40,7 @@ public class RNUtils {
                     hashMap.put(key, toHashMap(readableNativeMap.getMap(key)));
                     break;
                 case Array:
-                    hashMap.put(key, toArrayList(readableNativeMap.getArray(key)));
+                    hashMap.put(key, toArrayList((ReadableNativeArray)readableNativeMap.getArray(key)));
                     break;
                 default:
                     throw new IllegalArgumentException("Could not convert object with key: " + key + ".");
@@ -75,7 +75,7 @@ public class RNUtils {
                     arrayList.add(toHashMap(readableNativeArray.getMap(i)));
                     break;
                 case Array:
-                    arrayList.add(toArrayList(readableNativeArray.getArray(i)));
+                    arrayList.add(toArrayList((ReadableNativeArray)readableNativeArray.getArray(i)));
                     break;
                 default:
                     throw new IllegalArgumentException("Could not convert object at index: " + i + ".");
